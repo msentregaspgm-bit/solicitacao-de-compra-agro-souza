5240b060a1ee3dd9592c36723c8785f17f9f0303
msentregaspgm-bit <msentregaspgm@gmail.com>
2025-12-30 14:26:38 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwnCdlMJqU1zmTPGX8fupZXNjAev7jHAhXBrV0cPrQnN8_5WHMEJCU3i794DLvPTeyh/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxvbAHV1bORQLOJcl6F16kl4w0b99Uyu01Oda41QJeEki-5e5zEhTKvUyB6VI40hJH7/exec";
 
