7f2f0f4c74907807fea74d11975ca7ab25135b67
msentregaspgm-bit <msentregaspgm@gmail.com>
2026-01-05 15:07:42 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,3 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   const API_URL = "https://script.google.com/macros/s/AKfycbwUfuHDw9S67CRE6Ro9tTSe0lku_xZKRGBODN2Li0yzkplHkk9vasXOO_Q6gU-c7tdN/exec";
 
